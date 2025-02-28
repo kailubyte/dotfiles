fb2fb8de9fb5814e5663464231c7fb1fcd3cd11a
kailubyte <162659603+kailubyte@users.noreply.github.com>
2025-02-27 20:29:14 -0600

    clock item fixup

--- home/.config/sketchybar/items/clock.sh
+++ home/.config/sketchybar/items/clock.sh
@@ -10,4 +10,4 @@ clock=(
 
 sketchybar --add item clock right \
            --set clock "${clock[@]}" \
-           --subscribe clock clock_change mouse.clicked
+           --set clock update_freq=10 icon=  script="$PLUGIN_DIR/clock.sh" \
